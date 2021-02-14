5e81e3268fb4a3595fc554834251bb92ccd3b2e4
sqrniqer <68608815+sqrniqer@users.noreply.github.com>
2021-02-14 17:35:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const express = require("express")
 const app = express()
-let port = process.env.PORT || 3000
+let port = process.env.PORT || 4000
 const url = process.env.MONGODB_URI || 'mongodb+srv://Karol-Klimas:Szachy1234@sklepsruby.omqyc.mongodb.net/categories?retryWrites=true&w=majority'
 
 // Połączenie się z bazą danych
@@ -12,6 +12,15 @@ mongoose.connect(url, {
 
 // Importowanie modeli z ./models
 const Chemistry = require('./models/chemistry')
+const Accumulators = require('./models/accumulators')
+
+
+
+
+
+
+
+// Routes
 
 app.get('/api/chemistry', (req, res)=>{
     Chemistry.find()
@@ -21,8 +30,23 @@ app.get('/api/chemistry', (req, res)=>{
     })
     .catch((err)=>{ 
         console.log("Napotkano problem przy przesyłaniu danych z bazy danych do klienta API")
-        console.log(err)})
+        console.log(err)
+    })
 })
+
+app.get('/api/accumulators', (req, res)=>{
+    Accumulators.find()
+    .then((results)=>{
+        console.log(results)
+        res.send(results)
+    })
+    .catch((err)=>{ 
+        console.log("Napotkano problem przy przesyłaniu danych z bazy danych do klienta API")
+        console.log(err)
+    })
+})
+
+
 
 app.listen(port, ()=>{
     console.log(`API zostało uruchomione i działa pod adresem: http://localhost:${port}`)
